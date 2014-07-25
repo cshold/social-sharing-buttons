d9a88d9ad2db9a3d4d1ab1fc1aef91cd248ede40
Carson Shold <carson@carsonshold.com>
2014-07-25 11:35:42 -0400

    Cleaned up jquery selector variables

--- social-buttons.js
+++ social-buttons.js
@@ -9,7 +9,7 @@ window.CSbuttons = window.CSbuttons || {};
 
 $(function() {
   CSbuttons.cache = {
-    shareButtons: $('.social-sharing')
+    $shareButtons: $('.social-sharing')
   }
 });
 
@@ -18,54 +18,53 @@ CSbuttons.init = function () {
 }
 
 CSbuttons.socialSharing = function () {
-  var buttons = CSbuttons.cache.shareButtons,
-      permalink = buttons.attr('data-permalink'),
-      shareLinks = buttons.find('a').addClass('tester'),
-      socialCounts = buttons.find('span.share-count');
+  var $buttons = CSbuttons.cache.$shareButtons,
+      $shareLinks = $buttons.find('a'),
+      permalink = $buttons.attr('data-permalink');
 
   // Get share stats from respective APIs
-  var fbLink = $('.share-facebook'),
-      twitLink = $('.share-twitter'),
-      pinLink = $('.share-pinterest'),
-      googleLink = $('.share-google');
+  var $fbLink = $('.share-facebook'),
+      $twitLink = $('.share-twitter'),
+      $pinLink = $('.share-pinterest'),
+      $googleLink = $('.share-google');
 
-  if ( fbLink.length ) {
+  if ( $fbLink.length ) {
     $.getJSON('https://graph.facebook.com/?id=' + permalink + '&callback=?', function(data) {
       if (data.shares) {
-        fbLink.find('.share-count').text(data.shares).addClass('is-loaded');
+        $fbLink.find('.share-count').text(data.shares).addClass('is-loaded');
       } else {
-        fbLink.find('.share-count').remove();
+        $fbLink.find('.share-count').remove();
       }
     });
   };
 
-  if ( twitLink.length ) {
+  if ( $twitLink.length ) {
     $.getJSON('https://cdn.api.twitter.com/1/urls/count.json?url=' + permalink + '&callback=?', function(data) {
       if (data.count > 0) {
-        twitLink.find('.share-count').text(data.count).addClass('is-loaded');
+        $twitLink.find('.share-count').text(data.count).addClass('is-loaded');
       } else {
-        twitLink.find('.share-count').remove();
+        $twitLink.find('.share-count').remove();
       }
     });
   };
 
-  if ( pinLink.length ) {
+  if ( $pinLink.length ) {
     $.getJSON('https://api.pinterest.com/v1/urls/count.json?url=' + permalink + '&callback=?', function(data) {
       if (data.count > 0) {
-        pinLink.find('.share-count').text(data.count).addClass('is-loaded');
+        $pinLink.find('.share-count').text(data.count).addClass('is-loaded');
       } else {
-        pinLink.find('.share-count').remove();
+        $pinLink.find('.share-count').remove();
       }
     });
   };
 
-  if ( googleLink.length ) {
+  if ( $googleLink.length ) {
     // Can't currently get Google+ count with JS, so just pretend it loaded
-    googleLink.find('.share-count').addClass('is-loaded');
+    $googleLink.find('.share-count').addClass('is-loaded');
   }
 
   // Share popups
-  shareLinks.on('click', function(e) {
+  $shareLinks.on('click', function(e) {
     e.preventDefault();
     var el = $(this),
         popup = el.attr('class').replace('-','_'),
